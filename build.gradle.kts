c2b6705168d21f1679fec2e74686c6ecca88154f
모준서 <jayking@colosseum.kr>
2025-10-20 17:15:39 +0900

    chore: kotlin reflect api dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,13 @@ plugins {
     id("org.springframework.boot") version "3.4.8"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "1.9.25"
+    /**
+     * 자동으로 open keyword 생성
+     */
     kotlin("plugin.spring") version "1.9.25"
+    /**
+     * jpa 가 자동으로 no-args constructor 생성
+     */
     kotlin("plugin.jpa") version "1.9.25"
 }
 
@@ -38,6 +44,10 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation(kotlin("stdlib-jdk8"))
+    /**
+     * Spring Data JPA가 Kotlin 엔티티의 생성자를 분석할 때 Kotlin reflection API를 사용
+     */
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
 tasks.withType<Test> {
